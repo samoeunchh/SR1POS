8bc2a5b64d0f11210c30d18e252f35e2e09d4678
samoeun chhay <samoeunchh@gmail.com>
2021-11-08 09:48:36 +0700

    Scan Item on Sale Page

---
+++ SR1POS/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SR1POS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SR1POS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{code}")]
+        public async Task<IActionResult> GetProduct(string code)
+        {
+            var data = await (from p in _context.Product
+                        join pp in _context.ProductPrice on p.UnitId equals pp.UnitId
+                        where p.Barcode.Equals(code)
+                        select new
+                        {
+                            p.ProductName,
+                            p.UnitId,
+                            p.Cost,
+                            pp.Price,
+                            p.OnHand
+                        }).FirstOrDefaultAsync();
+            return Ok(data);
+        }
+    }
+}
--- SR1POS/wwwroot/js/site.js
+++ SR1POS/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var options = "";
+$.ajax({
+    method: "GET",
+    contentType: "application/json",
+    url: "/Products/GetUnits",
+    success: function (content) {
+        $.each(content, function (i, v) {
+            options += "<option value='" + v.unitId + "'>" + v.unitName + "</option>";
+        });
+    },
+    error: function (error) {
+        console.log(error);
+    }
+});
